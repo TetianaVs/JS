7170c2c2f5f8076392af7d97d494769a884f54e7
TetianaVs <tetiana.qait@gmail.com>
2022-04-25 22:40:26 +0300

    new commit

---
+++ Creating object copy inside function.js
@@ -0,0 +1,18 @@
+//object personOne
+const personOne = {
+    name: "Bob",
+    age: 21
+}
+
+//create function and create new object updatedPerson inside this function 
+//change age (+1)
+function increasePersonAge (person) {
+    const updatedPerson  = Object.assign ({}, person)
+    updatedPerson.age += 1
+    return updatedPerson
+}
+
+//call a function with an argument (persoOne) and assign it to new variable const updatedPersonOne
+const updatedPersonOne = increasePersonAge (personOne)
+console.log(personOne.age)
+console.log(updatedPersonOne.age)
